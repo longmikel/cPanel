bf8b45b8889e6a679867959c682bb4bfef1b4778
longmikel <longhm.luck@gmail.com>
2020-02-27 11:17:38 +0700

    settings for droid

---
+++ roundcube/skins/droid/settings.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+    "colors" => array(
+        "F44336",
+        "E91E63",
+        "9C27B0",
+
+        "673AB7",
+        "3F51B5",
+        "1976D2",
+
+        "039BE5",
+        "0097A7",
+        "009688",
+
+        "43A047",
+        "689F38",
+        "AFB42B",
+
+        "FBC02D",
+        "FFA000",
+        "EF6C00",
+
+        "E64A19",
+        "795548",
+        "757575",
+    ),
+    "default_color" => "1976D2",
+    "font_icons_toolbars" => true,
+    "icons" => "material",
+);
